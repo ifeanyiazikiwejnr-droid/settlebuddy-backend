5c142eea84fe13d6de7edacf170d344f26fc178c
Azikiwe Ifeanyi <ifeanyi.azikiwe.jnr@gmail.com>
2026-04-15 23:44:25 +0100

    fix cors for production

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const userRoutes = require('./routes/users');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: '*',
+  credentials: false,
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
